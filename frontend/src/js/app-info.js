ca53c220872175a36b8fa3f3d598715d18d519c4
Hyeong-Gon Jo <hg221089@gmail.com>
2026-06-12 21:39:51 +0900

    Release v0.4.1

--- frontend/src/js/app-info.js
+++ frontend/src/js/app-info.js
@@ -1,8 +1,8 @@
 export const bundledAppInfo = {
   name: "IDF Analyzer",
-  version: "0.4.0",
-  title: "IDF Analyzer v0.4.0",
-  outputFilename: "idf-analyzer-v0.4.0",
+  version: "0.4.1",
+  title: "IDF Analyzer v0.4.1",
+  outputFilename: "idf-analyzer-v0.4.1",
 };
 
 let cachedAppInfo = null;
